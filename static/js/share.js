029871c025843070a2371bdfe2ed7edd9ad3092d
is2js <tingstyle1@gmail.com>
2022-08-25 00:16:31 +0900

    feat: 결과 공유페이지 구현 중

--- static/js/share.js
+++ static/js/share.js
@@ -6,72 +6,37 @@ function shareKakao() {
 
   const shareTitle = '십이간지 연애유형 결과'
   const shareDesc = infoList[typeInAlt].name;
-  const shareImage = url + 'images/image-' + typeInAlt + '.png';
+  const shareImageURL = url + 'images/image-' + typeInAlt + '.png';
   const shareURL = url + 'shareResult.html?type' + encodeURIComponent(typeInAlt);
-
-
 
 
 
   Kakao.Share.sendDefault({
     objectType: 'feed',
     content: {
-      title: '오늘의 디저트',
-      description: '아메리카노, 빵, 케익',
+      title: shareTitle,
+      description: shareDesc,
       imageUrl:
-        'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+        shareImageURL,
       link: {
-        mobileWebUrl: 'https://developers.kakao.com',
-        androidExecutionParams: 'test',
+        mobileWebUrl: shareURL,
+        webUrl: shareURL
       },
     },
-    itemContent: {
-      profileText: 'Kakao',
-      profileImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-      titleImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-      titleImageText: 'Cheese cake',
-      titleImageCategory: 'Cake',
-      items: [
-        {
-          item: 'Cake1',
-          itemOp: '1000원',
-        },
-        {
-          item: 'Cake2',
-          itemOp: '2000원',
-        },
-        {
-          item: 'Cake3',
-          itemOp: '3000원',
-        },
-        {
-          item: 'Cake4',
-          itemOp: '4000원',
-        },
-        {
-          item: 'Cake5',
-          itemOp: '5000원',
-        },
-      ],
-      sum: '총 결제금액',
-      sumOp: '15000원',
-    },
-    social: {
-      likeCount: 10,
-      commentCount: 20,
-      sharedCount: 30,
-    },
+
     buttons: [
       {
-        title: '웹으로 이동',
+        title: '공유자 결과 확인~!',
         link: {
-          mobileWebUrl: 'https://developers.kakao.com',
+          mobileWebUrl: shareURL,
+          webUrl: shareURL
         },
       },
       {
-        title: '앱으로 이동',
+        title: '나도 참여~!',
         link: {
-          mobileWebUrl: 'https://developers.kakao.com',
+          mobileWebUrl: url,
+          webUrl: url
         },
       },
     ]
